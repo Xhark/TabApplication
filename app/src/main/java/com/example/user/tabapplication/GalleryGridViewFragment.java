b6e2b82227f5e37a4f932ba6e4039c6621e55af1
xhark <ekzm0204@gmail.com>
2015-12-29 17:03:37 +0900

    Implement GridView (Tab2) show PL Logos

--- app/src/main/java/com/example/user/tabapplication/GalleryGridViewFragment.java
+++ app/src/main/java/com/example/user/tabapplication/GalleryGridViewFragment.java
@@ -1,14 +1,46 @@
 package com.example.user.tabapplication;
 
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.user.tabapplication.adapter.GridViewAdapter;
+import com.example.user.tabapplication.adapter.ImageItem;
+
+import java.util.ArrayList;
 
 public class GalleryGridViewFragment extends Fragment {
+    private GridView gridView;
+    private GridViewAdapter gridAdapter;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         return inflater.inflate(R.layout.gallery_layout, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        gridView = (GridView) getActivity().findViewById(R.id.gridView);
+        gridAdapter = new GridViewAdapter(getActivity(), R.layout.grid_item_layout, getData());
+        gridView.setAdapter(gridAdapter);
+    }
+
+    // Prepare some dummy data for gridview
+    private ArrayList<ImageItem> getData() {
+        final ArrayList<ImageItem> imageItems = new ArrayList<>();
+        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+        for (int i = 0; i < imgs.length(); i++) {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
+            imageItems.add(new ImageItem(bitmap, "Image#" + i));
+        }
+        return imageItems;
     }
 }
